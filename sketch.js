ea1262ad6ea1221997fff00ea66d4558ea18c587
ranjansinghkol <38905611+ranjansinghkol@users.noreply.github.com>
2021-07-28 20:52:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var array = [25, 87, 18, 33, 53];
+console.log(array);
+
+function check(){
+  var sum = array[0] + array[1] + array[2] + array[3] + array[4];
+  console.log(sum) 
+}
+check()
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+
+}
+
+
+
+
